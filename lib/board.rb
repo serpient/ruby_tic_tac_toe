9d7a3b95ac359303f0e81d64cc613aa9a681c1e9
serpient <littlegangwolf@gmail.com>
2019-04-22 09:59:11 -0700

    add board methods

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,19 @@ class Board
     def initialize(size:)
         @size = size
         @max_spaces = size * size
-        @state = (1..max_spaces).to_a
+        @state = (1..max_spaces).map {|num| num = :empty }
+    end
+
+    def update(position:, token:)
+        state[position] = token
+        return state
+    end
+
+    def is_full?()
+        state.all? {|position| position != :empty }
+    end
+
+    def position_is_available?(position)
+        return state[position] == :empty
     end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,11 +11,54 @@ describe 'Board' do
         end
 
         it 'creates new board' do
-            expect(board.state).to eq [1,2,3,4,5,6,7,8,9]
+            expect(board.state).to eq [:empty,:empty,:empty,:empty,:empty,:empty,:empty,:empty,:empty]
         end
 
         it 'sets max spaces' do
             expect(board.max_spaces).to eq 9
         end
-    end 
+    end  
+
+    context 'Update' do
+        it 'updates a position in board state' do
+            board = Board.new(
+                size: 2
+            )
+            expect(board.update(
+                position: 0, 
+                token: :player
+            )).to eq [:player, :empty, :empty, :empty]
+        end
+    end
+
+    context 'Position_Is_Available' do
+        it 'returns true if position is :empty' do
+            board = Board.new(
+                size: 2
+            )
+            expect(board.position_is_available?(0)).to eq true
+        end
+    end
+
+    context 'Is_Full' do
+        it 'returns false when board is not full' do
+            board = Board.new(
+                size: 2
+            )
+            expect(board.is_full?()).to eq false
+        end
+
+        it 'returns true when board is full' do
+            board = Board.new(
+                size: 2
+            )
+
+            board.update(position: 0, token: :player)
+            board.update(position: 1, token: :player)
+            board.update(position: 2, token: :player)
+            board.update(position: 3, token: :player)
+
+            expect(board.is_full?()).to eq true
+        end
+    end
 end
