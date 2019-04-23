1281cb8acbd91105d0350b413f9b6f1228bc36b5
serpient <littlegangwolf@gmail.com>
2019-04-23 14:15:13 -0700

    create game setting interface
    - opponentType and boardSize are created to hook in.
    - should be easy to create other game setting types

---
+++ lib/game_settings.rb
@@ -0,0 +1,53 @@
+require_relative '../lib/validator'
+require_relative '../lib/messages'
+
+class GameSettings
+    attr_accessor :setting, :input
+
+    def initialize(setting:)
+        @setting = setting
+    end
+
+    def is_valid?(input: nil)
+        @input = input
+        setting.is_valid?(input: input)
+    end
+
+    def name()
+        setting.name
+    end
+
+    def message()
+        setting.message
+    end
+end
+
+class OpponentType
+    include Messages
+    include Validator
+    attr_accessor :name, :message
+    
+    def initialize()
+        @name = "opponent_type"
+        @message = opponent_type_msg()
+    end
+
+    def is_valid?(input: nil)
+        opponent_type_valid?(input: input)
+    end
+end
+
+class BoardSize
+    include Messages
+    include Validator
+    attr_accessor :name, :message
+    
+    def initialize()
+        @name = "board_size"
+        @message = board_size_msg()
+    end
+
+    def is_valid?(input: nil)
+        board_size_valid?(input: input)
+    end
+end
---
+++ spec/game_settings_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../lib/game_settings'
+
+describe 'Game Settings' do
+    context 'Initialize' do
+        game_settings = GameSettings.new(
+            setting: OpponentType.new()
+        )
+        it 'sets name' do
+            expect(game_settings.name).to eql 'opponent_type'
+        end
+        it 'sets setting class' do
+            expect(game_settings.setting.is_a?(OpponentType)).to eql true
+        end
+    end
+
+    context 'opponent type is_valid' do
+        game_settings = GameSettings.new(
+            setting: OpponentType.new(),
+        )
+        it 'returns true if valid' do
+            expect(game_settings.is_valid?(input: "H")).to eql true
+        end
+        it 'returns false if invalid' do
+            expect(game_settings.is_valid?(input: "3")).to eql false
+        end
+    end
+
+    context 'board size is_valid' do
+        game_settings = GameSettings.new(
+            setting: BoardSize.new(),
+        )
+        it 'returns false if invalid' do
+            expect(game_settings.is_valid?(input: "H")).to eql false
+        end
+        it 'returns true if valid' do
+            expect(game_settings.is_valid?(input: "3")).to eql true
+        end
+    end
+end
