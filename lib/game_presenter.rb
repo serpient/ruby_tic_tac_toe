61722df211d4e74d50a79556974547c2cc027891
serpient <littlegangwolf@gmail.com>
2019-04-23 14:16:47 -0700

    add additional test inputs to testIO
    - instead of returning a single value, we are now going to return a series of ordered values so when I ping against it multiple times, it returns an expected different number.
    - uses an iterator to keep track

--- lib/game_presenter.rb
+++ lib/game_presenter.rb
@@ -12,25 +12,59 @@ class GamePresenter
     def output_message(message:)
         presenter.output_message(message: message)
     end
+
+    def clear()
+        presenter.clear()
+    end
 end
 
 class ConsoleIO
     def get_input(input: nil)
-        print "Choose a position: "
         return gets.chomp
     end
 
     def output_message(message:)
         puts message
     end
+
+    def clear()
+        print "\e[2J\e[f"
+    end
 end
 
 class TestIO
+    attr_accessor :count
+
+    def initialize()
+        @count = 0
+    end
+
+    def update_count()
+        @count += 1
+    end
+
     def get_input(input: nil)
-        return input || "4"
+        return input if input != nil
+
+        value = case count 
+        when 0
+            "4"
+        when 1
+            "H"
+        when 2
+            "0"
+        else
+            "1"
+        end
+        update_count()
+        return value
     end
 
     def output_message(message:)
         message + "\n"
+    end
+
+    def clear()
+        return
     end
 end
