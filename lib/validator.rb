42956c125a9601e1a5eccc1983fb1132eb390620
serpient <littlegangwolf@gmail.com>
2019-04-24 09:04:20 -0700

    rename position validation

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,5 +1,5 @@
 module Validator
-    def position_input_valid?(input:, board:) 
+    def position_valid?(input:, board:) 
         int = input.to_i
         return board.is_input_valid?(int) && board.is_position_empty?(int)
     end
