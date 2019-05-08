d1ec8cc3592372ae3da5f2546b88e5d96bf4ca95
serpient <littlegangwolf@gmail.com>
2019-05-08 14:37:59 -0700

    change hash access to fetch

--- lib/persister/game_serializer.rb
+++ lib/persister/game_serializer.rb
@@ -20,7 +20,7 @@ class GameSerializer
 
     def collect_game_data(game)
         game_data_types.each.reduce({}) do |data_hash, type|
-            data_hash[type[:name]] = type[:data]
+            data_hash[type.fetch(:name)] = type.fetch(:data)
             data_hash
         end
     end
@@ -28,8 +28,8 @@ class GameSerializer
     def deserialize_data_fields(game_data)
         game_data_types.each do |type|
             if type[:deserialize]
-                value = game_data[type[:name]]
-                game_data[type[:name]] = type[:deserialize].(value)
+                value = game_data[type.fetch(:name)]
+                game_data[type.fetch(:name)] = type[:deserialize].(value)
             end
         end
         game_data
--- lib/persister/reinitializer.rb
+++ lib/persister/reinitializer.rb
@@ -5,9 +5,9 @@ module Reinitializer
     
     def generate(game_data:)
         @game = Game.new(
-            board_size: game_data[:board_size],
-            player_2: game_data[:player_2],
-            game_mode: game_data[:game_mode],
+            board_size: game_data.fetch(:board_size),
+            player_2: game_data.fetch(:player_2),
+            game_mode: game_data.fetch(:game_mode),
         )
         set_status(game_data)
         set_board_position(game_data)
@@ -20,19 +20,19 @@ module Reinitializer
     attr_accessor :game
 
     def set_status(game_data)
-        game.status = game_data[:status]
+        game.status = game_data.fetch(:status)
     end
 
     def set_board_position(game_data)
-        game.game_state.board.positions = game_data[:board_positions]
+        game.game_state.board.positions = game_data.fetch(:board_positions)
     end
 
     def set_current_player(game_data)
-        game.game_state.current_player = game_data[:current_player_token] == :X ? game.game_state.player_1 : game.game_state.player_2
+        game.game_state.current_player = game_data.fetch(:current_player_token) == :X ? game.game_state.player_1 : game.game_state.player_2
     end
 
     def set_lite_3_data(game_data)
-        game.game_state.game_mode.player_1_moves = game_data[:player_1_moves] 
-        game.game_state.game_mode.player_2_moves = game_data[:player_2_moves] 
+        game.game_state.game_mode.player_1_moves = game_data.fetch(:player_1_moves) 
+        game.game_state.game_mode.player_2_moves = game_data.fetch(:player_2_moves) 
     end
 end 
