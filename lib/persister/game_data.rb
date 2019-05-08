4991f9ad5007e090a096d73ac438beeb0f38e8d3
serpient <littlegangwolf@gmail.com>
2019-05-08 11:31:47 -0700

    change hash syntax to `name:` to match best practice

--- lib/persister/game_data.rb
+++ lib/persister/game_data.rb
@@ -24,53 +24,53 @@ class GameData
 
     def board_presenter
         {
-            :name => :board_presenter,
-            :data => game.board_presenter.presenter.class.name
+            name: :board_presenter,
+            data: game.board_presenter.presenter.class.name
         }
     end
 
     def status
         {
-            :name => :status,
-            :data => game.status,
-            :deserialize => -> (string) { convert_to_symbol(string) }
+            name: :status,
+            data: game.status,
+            deserialize: -> (string) { convert_to_symbol(string) }
         }
     end
 
     def game_mode
         {
-            :name => :game_mode,
-            :data => game.game_state.game_mode.class.name,
+            name: :game_mode,
+            data: game.game_state.game_mode.class.name,
         }
     end
 
     def board_positions
         {
-            :name => :board_positions,           
-            :data => game.game_state.board.positions,
-            :deserialize => -> (array) { array.map { |string| convert_to_symbol(string) } },
+            name: :board_positions,           
+            data: game.game_state.board.positions,
+            deserialize: -> (array) { array.map { |string| convert_to_symbol(string) } },
         }
     end
 
     def board_size
         {
-            :name => :board_size,   
-            :data => game.game_state.board.size,
+            name: :board_size,   
+            data: game.game_state.board.size,
         }
     end
 
     def current_player_type
         {
-            :name => :current_player_type,   
-            :data => game.game_state.current_player.player.class.name,
+            name: :current_player_type,   
+            data: game.game_state.current_player.player.class.name,
         }
     end
 
     def current_player_token
         {
-            :name => :current_player_token,   
-            :data => game.game_state.current_player.player.token,
-            :deserialize => -> (string) { convert_to_symbol(string) }
+            name: :current_player_token,   
+            data: game.game_state.current_player.player.token,
+            deserialize: -> (string) { convert_to_symbol(string) }
         }
     end
 
@@ -78,18 +78,18 @@ class GameData
     def player_1_moves
         lite_3_mode = game.game_state.game_mode.is_a?(Lite3)
         {
-            :name => :player_1_moves,   
-            :data => lite_3_mode && struct_to_hash(game.game_state.game_mode.player_1_moves),
-            lite_3_mode && :deserialize => -> (array) { hash_to_struct(array) }
+            name: :player_1_moves,   
+            data: lite_3_mode && struct_to_hash(game.game_state.game_mode.player_1_moves),
+            deserialize: -> (array) { hash_to_struct(array) }
         }
     end 
 
     def player_2_moves
         lite_3_mode = game.game_state.game_mode.is_a?(Lite3)
         {
-            :name => :player_2_moves,  
-            :data => lite_3_mode && struct_to_hash(game.game_state.game_mode.player_2_moves),
-            lite_3_mode && :deserialize => -> (array) { hash_to_struct(array) }
+            name: :player_2_moves,  
+            data: lite_3_mode && struct_to_hash(game.game_state.game_mode.player_2_moves),
+            deserialize: -> (array) { hash_to_struct(array) }
         }
     end 
 
@@ -102,6 +102,7 @@ class GameData
     end
     
     def hash_to_struct(array)
+        return array if !array
         lite_3 = Lite3.new
         array.map { |hash| lite_3.new_position(token: convert_to_symbol(hash[:token]), position: hash[:position]) }
     end
